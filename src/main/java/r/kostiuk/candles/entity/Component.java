b2740d9e144799cf5eb7c2847eb1fa65720e0a62
Ruslan Kostiuk <rsln.kostuk@gmail.com>
2023-08-30 00:50:30 +0300

    Add Component and ComponentType entities

---
+++ src/main/java/r/kostiuk/candles/entity/Component.java
@@ -0,0 +1,52 @@
+package r.kostiuk.candles.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.NaturalId;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+@NoArgsConstructor
+@ToString
+@Getter
+@Setter
+@Entity
+@Table(name = "components")
+public class Component implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NaturalId
+    @Column(nullable = false)
+    private String name;
+
+    @Column(name = "amount_in_stock", columnDefinition = "numeric(12, 3)")
+    private BigDecimal amountInStock = BigDecimal.ZERO;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "type_id")
+    private ComponentType type;
+
+    public Component(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Component component)) {
+            return false;
+        }
+        return Objects.equals(name, component.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
---
+++ src/main/java/r/kostiuk/candles/entity/ComponentType.java
@@ -0,0 +1,64 @@
+package r.kostiuk.candles.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.NaturalId;
+
+import java.io.Serializable;
+import java.util.*;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "component_types")
+public class ComponentType implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NaturalId
+    @Column(nullable = false)
+    private String name;
+
+    @Setter(AccessLevel.NONE)
+    @OneToMany(mappedBy = "type", cascade = CascadeType.PERSIST)
+    private Set<Component> components = new HashSet<>();
+
+    public ComponentType(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof ComponentType type)) {
+            return false;
+        }
+        return Objects.equals(name, type.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        return "ComponentType{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public Collection<Component> getComponents() {
+        return Collections.unmodifiableSet(components);
+    }
+
+    public void addComponent(Component component) {
+        component.setType(this);
+        components.add(component);
+    }
+}
