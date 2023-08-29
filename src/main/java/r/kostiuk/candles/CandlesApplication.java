801250fa0e0d7c64d2aa9149b1b0a1fa38f3dca4
Ruslan Kostiuk <rsln.kostuk@gmail.com>
2023-08-29 23:59:39 +0300

    Init app

---
+++ src/main/java/r/kostiuk/candles/CandlesApplication.java
@@ -0,0 +1,13 @@
+package r.kostiuk.candles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandlesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CandlesApplication.class, args);
+    }
+
+}
