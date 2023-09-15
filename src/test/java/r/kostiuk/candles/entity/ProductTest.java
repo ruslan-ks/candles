eaac58170e12127d270a467dcaf8e48102664022
Ruslan Kostiuk <rsln.kostuk@gmail.com>
2023-09-15 13:21:31 +0300

    Refactor ProductTest: add method createProduct

--- src/test/java/r/kostiuk/candles/entity/ProductTest.java
+++ src/test/java/r/kostiuk/candles/entity/ProductTest.java
@@ -12,7 +12,7 @@ import static org.assertj.core.api.Assertions.*;
 class ProductTest {
     @Test
     void addMultipleIncludedProducts() {
-        var compoundProduct = new Product("Compound Product", BigDecimal.ONE);
+        var compoundProduct = createProduct("Compound Product", BigDecimal.ONE);
         List<Product> simpleProducts = TestData.generateProducts(5).toList();
 
         simpleProducts.forEach(compoundProduct::addIncludedProduct);
@@ -23,8 +23,8 @@ class ProductTest {
 
     @Test
     void addIncludedProductUpdatesBothSidesOfRelation() {
-        var simpleProduct = new Product("Simple Product", BigDecimal.ONE);
-        var compoundProduct = new Product("Compound Product", BigDecimal.TEN);
+        var simpleProduct = createProduct("Simple Product", BigDecimal.ONE);
+        var compoundProduct = createProduct("Compound Product", BigDecimal.TEN);
 
         compoundProduct.addIncludedProduct(simpleProduct);
 
@@ -34,12 +34,16 @@ class ProductTest {
 
     @Test
     void includeProductIntoMultipleOtherProducts() {
-        var simpleProduct = new Product("Compound Product", BigDecimal.ONE);
+        var simpleProduct = createProduct("Compound Product", BigDecimal.ONE);
         List<Product> compoundProducts = TestData.generateProducts(5).toList();
 
         compoundProducts.forEach(compoundProduct -> compoundProduct.addIncludedProduct(simpleProduct));
 
         Set<Product> productsIncludingSimpleProduct = simpleProduct.getProductsIncludingThis();
         assertThat(productsIncludingSimpleProduct).containsExactlyInAnyOrderElementsOf(compoundProducts);
+    }
+
+    private Product createProduct(String name, BigDecimal price) {
+        return new Product(name, price);
     }
 }
