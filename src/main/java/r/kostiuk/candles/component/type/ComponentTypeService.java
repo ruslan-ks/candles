1a4236c50cea1c81631ed854fecf3638eb1052e1
Ruslan Kostiuk <rsln.kostuk@gmail.com>
2023-10-08 23:12:38 +0300

    Add ComponentTypeService

---
+++ src/main/java/r/kostiuk/candles/component/type/ComponentTypeService.java
@@ -0,0 +1,14 @@
+package r.kostiuk.candles.component.type;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import r.kostiuk.candles.component.type.dto.ComponentTypeResponse;
+import r.kostiuk.candles.component.type.dto.NewComponentTypeRequest;
+import r.kostiuk.candles.component.type.exception.ComponentTypeNotFoundException;
+
+public interface ComponentTypeService {
+    Page<ComponentTypeResponse> findPage(Pageable pageable);
+    ComponentTypeResponse create(NewComponentTypeRequest newComponentRequest);
+    void deleteById(int componentTypeId) throws ComponentTypeNotFoundException;
+    ComponentType findById(int componentTypeId) throws ComponentTypeNotFoundException;
+}
---
+++ src/main/java/r/kostiuk/candles/component/type/dto/NewComponentTypeRequest.java
@@ -0,0 +1,9 @@
+package r.kostiuk.candles.component.type.dto;
+
+import lombok.Data;
+
+@Data
+public class NewComponentTypeRequest {
+    private String name;
+    private String measurement;
+}
