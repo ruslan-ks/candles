d7a4a59583ecc4cc0774ce561397cd4af8c4b93c
Ruslan Kostiuk <rsln.kostuk@gmail.com>
2023-10-16 00:55:01 +0300

    Complete ComponentTypeValidator tests

--- src/main/java/r/kostiuk/candles/type/validation/ComponentTypeControllerValidator.java
+++ src/main/java/r/kostiuk/candles/type/validation/ComponentTypeControllerValidator.java
@@ -4,9 +4,14 @@ import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import r.kostiuk.candles.exception.ControllerValidationException;
 import r.kostiuk.candles.exception.CustomValidationException;
+import r.kostiuk.candles.messages.MessageProvider;
 
 @Component
 public class ComponentTypeControllerValidator extends ComponentTypeValidator {
+    public ComponentTypeControllerValidator(MessageProvider messageProvider) {
+        super(messageProvider);
+    }
+
     @Override
     protected CustomValidationException createValidationException(Errors errors) {
         return new ControllerValidationException(errors);
