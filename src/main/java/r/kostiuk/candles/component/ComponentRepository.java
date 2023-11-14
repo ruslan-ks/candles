fc36377bb67e837fc20ea76e8ff3366c5ae8a8e2
Ruslan Kostiuk <rsln.kostuk@gmail.com>
2023-11-14 16:59:37 +0200

    Make ComponentRepository extend JpaSpecificationExecutor; remove  findComponentPage method

--- src/main/java/r/kostiuk/candles/component/ComponentRepository.java
+++ src/main/java/r/kostiuk/candles/component/ComponentRepository.java
@@ -1,18 +1,9 @@
 package r.kostiuk.candles.component;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
-import r.kostiuk.candles.component.dto.ComponentResponse;
 
 @Repository
-public interface ComponentRepository extends JpaRepository<Component, Long> {
-    @Query("""
-            SELECT new r.kostiuk.candles.component.dto.ComponentResponse(c.id, c.name, t.name)
-            FROM Component c
-            JOIN c.type t
-            """)
-    Page<ComponentResponse> findComponentsPage(Pageable pageable);
+public interface ComponentRepository extends JpaRepository<Component, Long>, JpaSpecificationExecutor<Component> {
 }
